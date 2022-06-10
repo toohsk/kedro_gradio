e6105103ef2acb9a895a50ac028f5c4ad9103b45
toohsk <toohsk@gmail.com>
2022-06-10 09:35:30 +0900

    Update pipeline_registry.

--- kedro-gradio/src/kedro_gradio/pipeline_registry.py
+++ kedro-gradio/src/kedro_gradio/pipeline_registry.py
@@ -4,6 +4,7 @@ from typing import Dict
 from kedro.pipeline import Pipeline, pipeline
 
 from kedro_gradio.pipelines import data_processing as dp
+from kedro_gradio.pipelines import data_science as ds
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -13,8 +14,10 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     data_processing_pipeline = dp.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
 
     return {
-        "__default__": data_processing_pipeline,
+        "__default__": data_processing_pipeline + data_science_pipeline,
         "dp": data_processing_pipeline,
+        "ds": data_science_pipeline,
     }
